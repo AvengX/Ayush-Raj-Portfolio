377d765c2c34dd4eac7235a17e86a00151adac73
AvengX <ayushraj7654321@gmail.com>
2026-05-20 22:00:50 +0530

    Update Tech Stack bubbles to match resume

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -14,13 +14,15 @@ import {
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
   "/images/react2.webp",
-  "/images/next2.webp",
-  "/images/node2.webp",
-  "/images/express.webp",
-  "/images/mongo.webp",
-  "/images/mysql.webp",
-  "/images/typescript.webp",
   "/images/javascript.webp",
+  "/images/mysql.webp",
+  "/images/python.svg",
+  "/images/c.svg",
+  "/images/cplusplus.svg",
+  "/images/html5.svg",
+  "/images/css3.svg",
+  "/images/tailwindcss.svg",
+  "/images/git.svg",
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
